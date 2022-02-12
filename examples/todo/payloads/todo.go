4276339d308854b72b72389cf87dcb7bf127c36c
Giorgos Komninos <admin@gkomninos.com>
2022-02-12 21:15:45 +0100

    removes example folder

--- examples/todo/payloads/todo.go
+++
@@ -1,6 +0,0 @@
-package payloads
-
-type CreateTodoPayload struct {
-	Title   string `json:"title"`
-	Content string `json:"content"`
-}
--- examples/todo/schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  identifier varchar(50) NOT NULL UNIQUE
-);
